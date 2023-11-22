4cd44fe3f19f8317e5ce4372fdc812187f2e3b3d
Rajeev721 <rajeev.suravajhula@gmail.com>
2023-11-22 00:52:09 -0600

    commit

--- imdb_etl.py
+++ imdb_etl.py
@@ -3,41 +3,56 @@ import json
 import pandas as pd
 import os
 import boto3
-# import pprint
-api_key = os.environ['IMBD_API_KEY']
-api_host = os.environ.get('IMDB_API_HOST')
-headers = {
-	"X-RapidAPI-Key": api_key,
-	"X-RapidAPI-Host": api_host
-}
-"""# #This api call is to get reviews
-# rev_url = "https://imdb8.p.rapidapi.com/title/get-reviews"
-# rev_querystring = {"tconst":"tt0944947","currentCountry":"US","purchaseCountry":"US"}
-# #This api call is to get user reviews
-# url = "https://imdb8.p.rapidapi.com/title/get-user-reviews"
-# querystring = {"tconst":"tt0944947"}
-# rev_response = requests.get(rev_url, headers=headers, params= rev_querystring)
-# user_rev_response = requests.get(url, headers=headers, params= querystring)
-"""
 
-get_all_images_url = "https://imdb8.p.rapidapi.com/title/get-all-images"
-querystring = {"tconst":"tt0944947"}
-get_all_images_res = requests.get(get_all_images_url, headers=headers, params= querystring)
+def rapid_api_etl():
+	try:
+		api_key = os.environ['IMBD_API_KEY']
+		api_host = os.environ.get('IMDB_API_HOST')
+		headers = {
+			"X-RapidAPI-Key": api_key,
+			"X-RapidAPI-Host": api_host
+		}
+		"""****************************************Check if HTTPD is installed in the server if you are unable to connect via public IP****************************************
+		yum install httpd
+		getenforce
+		service httpd start
+		"""
+		"""****************************************Installing Python and airflow:****************************************
+		sudo yum install update
+		sudo yum install python3-pip
+		pip install "apache-airflow[celery]==2.7.3" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.7.3/constraints-3.8.txt"
+		airflow standalone
+		"""
+		"""****************************************This api call is to get reviews****************************************
+		# rev_url = "https://imdb8.p.rapidapi.com/title/get-reviews"
+		# rev_querystring = {"tconst":"tt0944947","currentCountry":"US","purchaseCountry":"US"}
+		# #This api call is to get user reviews
+		# url = "https://imdb8.p.rapidapi.com/title/get-user-reviews"
+		# querystring = {"tconst":"tt0944947"}
+		# rev_response = requests.get(rev_url, headers=headers, params= rev_querystring)
+		# user_rev_response = requests.get(url, headers=headers, params= querystring)
+		"""
+
+		get_all_images_url = "https://imdb8.p.rapidapi.com/title/get-all-images"
+		querystring = {"tconst":"tt0944947"}
+		get_all_images_res = requests.get(get_all_images_url, headers=headers, params= querystring)
 
 
-review_response = get_all_images_res.json()
+		review_response = get_all_images_res.json()
 
-images_dict = {}
+		images_dict = {}
 
-for image in review_response.get('resource')['images']:
-	images_dict[image.get('id').split('/')[-1]] = {"caption" : image.get('caption'),
-	"created_on" : image.get('createdOn'),
-	"url" : image.get('url'),
-	"name" : image.get('relatedNames')[0]['name']}
+		for image in review_response.get('resource')['images']:
+			images_dict[image.get('id').split('/')[-1]] = {"caption" : image.get('caption'),
+			"created_on" : image.get('createdOn'),
+			"url" : image.get('url'),
+			"name" : image.get('relatedNames')[0]['name']}
 
-# print(review_response.get('@meta')['requestId'])
-# print(json.dumps(images_dict,  indent=4))
+		# print(review_response.get('@meta')['requestId'])
+		# print(json.dumps(images_dict,  indent=4))
 
 
-with open('images_dict.json', 'w') as f:
-	json.dump(images_dict,f)
+		with open('s3://rajeevlearning/landing/images_dict.json', 'w') as f:
+			json.dump(images_dict,f)
+	except Exception:
+		print(Exception)
---
+++ rapid_api_dags/imdb_dag.py
@@ -0,0 +1,30 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+import sys
+ 
+# adding Folder_2 to the system path
+sys.path.insert(0, '/home/hduser/git/imdb_to_s3_airflow')
+from imdb_etl import rapid_api_etl
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG('rapid_api', description="First Airflow Dag to load data",default_args= default_args, schedule_interval= timedelta(days=1) )
+
+run_etl = PythonOperator(
+    task_id='rapidapi_etl',
+    python_callable=rapid_api_etl,
+    dag=dag, 
+)
+
+run_etl
